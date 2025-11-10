21997a7e7bbe326788f817fefa9ea1197b1b537c
ivan_brewis <5161291+ikb42@users.noreply.github.com>
2025-11-10 21:12:16 +0000

    ver bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lightwave_smart",
-    version="2.0.1",
+    version="2.0.2",
     author="Bryan Blunt / Lightwave",
     author_email="dev@lightwaverf.com",
     description="Controls for Lightwave Smart Series (second generation) devices",
